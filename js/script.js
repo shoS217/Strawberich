c949f8991e28678ca729c1034b5d4c136e1cb435
sho517 <s.2000top1-sho@i.softbank.jp>
2026-08-23 23:32:42 +0900

    ホームの各項目に遷移するボタンを作成
    ・ホームに遷移
    ・メニューに遷移
    ・店情報に遷移

--- js/script.js
+++ js/script.js
@@ -4,6 +4,12 @@ Navi
 const menuOpen = document.querySelector('.navmenu-open-btn');
 const menuPanel = document.querySelector('.navmenu-panel');
 const menuClose = document.querySelector('.navmenu-close-btn');
+const homeTransitionBtn = document.getElementById('home-transition-btn');
+const homeSection = document.getElementById('about-wrapper');
+const menuTransitionBtn = document.getElementById('menu-transition-btn');
+const menuSection = document.getElementById('menu-wrapper');
+const accessTransitionBtn = document.getElementById('access-transition-btn');
+const accessSection = document.getElementById('information-wrapper');
 const menuOpenKeyframe = 
 {
     translate: ['100vw', 0],
@@ -26,4 +32,28 @@ menuOpen.addEventListener('click', ()=>
 menuClose.addEventListener('click', ()=>
 {
     menuPanel.animate(menuCloseKeyframe, menuOptions)
+});
+homeTransitionBtn.addEventListener('click', () =>
+{
+    menuPanel.animate(menuCloseKeyframe, menuOptions);
+    homeSection.scrollIntoView(
+    {
+        behavior: "smooth",
+    });
+});
+menuTransitionBtn.addEventListener('click', ()=>
+{
+    menuPanel.animate(menuCloseKeyframe, menuOptions);
+    menuSection.scrollIntoView(
+    {
+        behavior: "smooth",
+    });
+});
+accessTransitionBtn.addEventListener('click', ()=>
+{
+    menuPanel.animate(menuCloseKeyframe, menuOptions);
+    accessSection.scrollIntoView(
+    {
+        behavior: "smooth",
+    });
 });
